27f610e74ed2f5e4cae66c999177d627f10a00a7
BiscuitsLayer <vinogradov.so@phystech.edu>
2020-10-07 22:25:49 +0300

    Improved

--- task3/reader.cpp
+++ task3/reader.cpp
@@ -7,18 +7,50 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <limits.h>
 
-const int MAX_SIZE = 1e6;
+const int MAX_SIZE = PIPE_BUF;
 
 int main () {
-    //flags:    O_RDONLY
-    FILE* readfile = fopen ("channel.fifo", "r");
+    if (mkfifo ("service.fifo", 0777) == -1) {
+        fprintf (stderr, "Error creating service fifo\n");
+        exit (EXIT_FAILURE);
+    }
+
+    if (mkfifo ("channel.fifo", 0777) == -1) {
+        fprintf (stderr, "Error creating channel fifo\n");
+        exit (EXIT_FAILURE);
+    }
+
+    int readfile = open ("channel.fifo", O_RDONLY | O_NONBLOCK);
+    if (readfile == -1) {
+        fprintf (stderr, "Error opening file\n");
+        exit (EXIT_FAILURE);
+    }
     char *buf = (char *) calloc (MAX_SIZE, sizeof (char));
+    
     fprintf (stdout, "INPUT WHEN READY TO START (READER)\n");
     fscanf (stdin, "%s", buf);
-    if (fscanf (readfile, "%s", buf) == 1)
-        fprintf (stderr, "READ SUCCESS\n");
-    else
-        fprintf (stderr, "READ FAIL\n");
-    return 0;
+
+    int num = 0;
+
+    while ((num = read (readfile, buf, PIPE_BUF)), num >= 0) {
+        if (num == PIPE_BUF) 
+            fprintf (stderr, "READ SUCCESS\n");
+        else if (num == 0) {
+            fprintf (stderr, "ZERO\n");
+            break;
+        } 
+        else 
+            fprintf (stderr, "FAILURE\n");
+        sleep (1);
+    }
+    
+    if (num == -1 && errno == EAGAIN) {
+    //  EAGAIN <- сейчас нет данных в FIFO
+        fprintf (stdout, "Writer has been broken\n");
+    }
+    close (readfile);
+    unlink ("channel.fifo");
+    exit (EXIT_SUCCESS);
 }
--- task3/writer.cpp
+++ task3/writer.cpp
@@ -7,21 +7,49 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <limits.h>
 
 int main () {
-    //flags:    O_WRONLY | O_CREAT | O_TRUNC
-    FILE *writefile = fopen ("channel.fifo", "w");   //in case of fifo O_TRUNC flag is ignored
+    char msg [PIPE_BUF];
+    for (unsigned i = 0; i < PIPE_BUF; ++i)
+        msg[i] = 'a' + rand() % 20;
+    msg[PIPE_BUF - 1] = '\0';
 
-    if (!writefile) {
-        fprintf (stderr, "Error opening file\n");
+    int writefile = open ("channel.fifo", O_WRONLY | O_NONBLOCK);
+
+    while (writefile == -1) {
+        if (errno == ENXIO) {
+            //  ENXIO <- никто не открыл FIFO для чтения
+            fprintf (stdout, "Reader has been broken\n");
+            exit (EXIT_FAILURE);
+        }
+        //  EEXIST <- файл не существует
+        writefile = open ("channel.fifo", O_WRONLY | O_NONBLOCK);
     }
 
     char buf = '\0';
+
     fprintf (stdout, "INPUT WHEN READY TO START (WRITER)\n");
     fscanf (stdin, "%c", &buf);
-    if (fprintf (writefile, "%s", "abc") == 3)
-        fprintf (stderr, "WRITE SUCCESS\n");
-    else
-        fprintf (stderr, "WRITE FAIL\n");
-    return 0;
+
+    int num = 0;
+    unsigned cnt = 0;
+
+    while (true) {
+        num = write (writefile, msg, PIPE_BUF);
+
+        if (num == PIPE_BUF) {
+            fprintf (stderr, "WRITE SUCCESS\n");
+        }
+        else {
+            fprintf (stderr, "WRITE FAIL, write returned: %d\n", num);
+
+        }
+        ++cnt;
+        sleep (1);
+    }
+
+    close (writefile);
+    unlink ("channel.fifo");
+    exit (EXIT_SUCCESS);
 }
