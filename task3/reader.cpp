210ca6c51f336f76c6ea643000e561b427f99e20
BiscuitsLayer <vinogradov.so@phystech.edu>
2020-10-08 11:46:43 +0300

    Trying out service FIFO

--- task3/reader.cpp
+++ task3/reader.cpp
@@ -9,25 +9,46 @@
 #include <errno.h>
 #include <limits.h>
 
+const int FILENAME_SIZE = 20;
 const int MAX_SIZE = PIPE_BUF;
+const char SUCCESS_CONNECT [] = "1";
 
 int main () {
-    if (mkfifo ("service.fifo", 0777) == -1) {
-        fprintf (stderr, "Error creating service fifo\n");
-        exit (EXIT_FAILURE);
-    }
 
-    if (mkfifo ("channel.fifo", 0777) == -1) {
+    pid_t pid = getpid ();
+    char channelName [FILENAME_SIZE];
+    sprintf (channelName, "channel_%d.fifo", pid);
+
+    if (mkfifo (channelName, 0777) == -1) {
         fprintf (stderr, "Error creating channel fifo\n");
         exit (EXIT_FAILURE);
     }
 
-    int readfile = open ("channel.fifo", O_RDONLY | O_NONBLOCK);
+    int readfile = open (channelName, O_RDONLY | O_NONBLOCK);
     if (readfile == -1) {
-        fprintf (stderr, "Error opening file\n");
+        fprintf (stderr, "Error opening channel fifo\n");
         exit (EXIT_FAILURE);
     }
-    char *buf = (char *) calloc (MAX_SIZE, sizeof (char));
+
+    //char serviceName [FILENAME_SIZE];
+    //serviceName = find
+    int serviceWritefile = open ("service.fifo", O_WRONLY | O_NONBLOCK);
+    while (serviceWritefile == -1) {
+        if (errno == ENXIO) {
+            //  ENXIO <- никто не открыл FIFO для чтения
+            fprintf (stdout, "Writer service connection failure\n");
+            exit (EXIT_FAILURE);
+        }
+        //  EEXIST <- файл не существует
+        serviceWritefile = open ("service.fifo", O_WRONLY | O_NONBLOCK);
+    }
+    fprintf (stdout, "%d\n", serviceWritefile);
+    
+    write (serviceWritefile, SUCCESS_CONNECT, 1);
+
+    //int fdFlags = fcntl (readfile, F_GETFD);
+
+    char buf [MAX_SIZE];
     
     fprintf (stdout, "INPUT WHEN READY TO START (READER)\n");
     fscanf (stdin, "%s", buf);
@@ -50,6 +71,7 @@ int main () {
     //  EAGAIN <- сейчас нет данных в FIFO
         fprintf (stdout, "Writer has been broken\n");
     }
+
     close (readfile);
     unlink ("channel.fifo");
     exit (EXIT_SUCCESS);
--- task3/writer.cpp
+++ task3/writer.cpp
@@ -9,14 +9,39 @@
 #include <errno.h>
 #include <limits.h>
 
+const int FILENAME_SIZE = 20;
+
 int main () {
+
+    pid_t pid = getpid ();
+    char serviceName [FILENAME_SIZE];
+    sprintf (serviceName, "channel_%d.fifo", pid);
+
     char msg [PIPE_BUF];
     for (unsigned i = 0; i < PIPE_BUF; ++i)
         msg[i] = 'a' + rand() % 20;
     msg[PIPE_BUF - 1] = '\0';
 
-    int writefile = open ("channel.fifo", O_WRONLY | O_NONBLOCK);
+    if (mkfifo (serviceName, 0777) == -1) {
+        fprintf (stderr, "Error creating service fifo\n");
+        exit (EXIT_FAILURE);
+    }
 
+    int serviceReadfile = open (serviceName, O_RDONLY | O_NONBLOCK);
+    if (serviceReadfile == -1) {
+        fprintf (stderr, "Error opening service fifo\n");
+        exit (EXIT_FAILURE);
+    }
+
+    int openSuccededCount = 0;
+    int openSuccededPid = 0;
+
+    while (openSuccededCount == 0) {
+        if (read (serviceReadfile, &openSuccededPid, 4) == 1)
+            openSuccededCount++;
+    }
+
+    int writefile = open ("channel.fifo", O_WRONLY | O_NONBLOCK);
     while (writefile == -1) {
         if (errno == ENXIO) {
             //  ENXIO <- никто не открыл FIFO для чтения
@@ -26,7 +51,12 @@ int main () {
         //  EEXIST <- файл не существует
         writefile = open ("channel.fifo", O_WRONLY | O_NONBLOCK);
     }
-
+    //  В этот момент можно запустить второй writer, который будет печатать в ту же FIFO
+    //  Чтобы избежать такого, нужно отправить через второй FIFO в обратную сторону (write
+    //  конец NON_BLOCK) сообщение об успешном коннекте
+    sleep (5);
+    //
+    chmod ("channel.fifo", 0000);
     char buf = '\0';
 
     fprintf (stdout, "INPUT WHEN READY TO START (WRITER)\n");
@@ -43,9 +73,10 @@ int main () {
         }
         else {
             fprintf (stderr, "WRITE FAIL, write returned: %d\n", num);
-
         }
         ++cnt;
+        if (cnt == 4)
+            break;
         sleep (1);
     }
 
